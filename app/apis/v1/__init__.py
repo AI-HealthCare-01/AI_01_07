2afe560f72783b60640d4aaac3eea2156f1b6852
ygrace3442 <ygrace3442@gmail.com>
2026-03-10 10:33:24 +0900

    added feature food analysis

--- app/apis/v1/__init__.py
+++ app/apis/v1/__init__.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from app.apis.v1.admin_routers import admin_router
 from app.apis.v1.auth_routers import auth_router
 from app.apis.v1.dashboard_routers import dashboard_router
+from app.apis.v1.food_routers import food_router
 from app.apis.v1.health_record_routers import health_record_router
 from app.apis.v1.meal_routers import meal_router
 from app.apis.v1.onboarding_routers import onboarding_router
@@ -15,6 +16,7 @@ v1_routers.include_router(auth_router)
 v1_routers.include_router(health_record_router)
 v1_routers.include_router(user_router)
 v1_routers.include_router(dashboard_router)
+v1_routers.include_router(food_router)
 v1_routers.include_router(prediction_router)
 v1_routers.include_router(meal_router)
 v1_routers.include_router(onboarding_router)
